672242eab72ac31f8b667aca4b9d510b22821d45
redrich2032 <ratcherdnewin2032@gmail.com>
2021-06-24 20:20:05 -0700

    Revert "custom welcome toolbar"
    This reverts commit 15bcdb98745c073ffaf0adfea10755ffc6d64d58.

--- app/src/main/java/com/example/project1/Welcome.java
+++ app/src/main/java/com/example/project1/Welcome.java
@@ -1,50 +1,23 @@
 package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Welcome extends AppCompatActivity {
 
     private TextView txtMessage;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         // Passes info from previous activity and outputs welcome message
         txtMessage = findViewById(R.id.message);
         String username = getIntent().getStringExtra("username");
-        txtMessage.setText(username);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Context context = getApplicationContext();
-        String selection = item.getTitle().toString();
-        if(selection.equals("developer info")) {
-            Toast.makeText(context,"developer info selected", Toast.LENGTH_SHORT).show();
-        }
-        return true;
+        txtMessage.setText("Welcome " + username + "!");
     }
 }
