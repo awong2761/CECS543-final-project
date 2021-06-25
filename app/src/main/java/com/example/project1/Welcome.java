d2d4ee3f72a8f6642d91649942566d231b71ff35
redrich2032 <ratcherdnewin2032@gmail.com>
2021-06-24 20:28:04 -0700

    Revert "Revert "Username from dev to home page""
    This reverts commit 3e64504104866e5f8737cedff9a492b7af2bb63c.

--- app/src/main/java/com/example/project1/Welcome.java
+++ app/src/main/java/com/example/project1/Welcome.java
@@ -1,8 +1,11 @@
 package com.example.project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
@@ -14,20 +17,23 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Welcome extends AppCompatActivity {
 
     private TextView txtMessage;
     private Toolbar toolbar;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Passes info from previous activity and outputs welcome message
         txtMessage = findViewById(R.id.message);
-        String username = getIntent().getStringExtra("username");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        username = getIntent().getStringExtra("username");
         txtMessage.setText(username);
 
     }
@@ -44,8 +50,10 @@ public class Welcome extends AppCompatActivity {
         String selection = item.getTitle().toString();
         if(selection.equals("developer info")) {
             Intent devInfo = new Intent(context, DevInfo.class);
+            devInfo.putExtra("username", username);
             startActivity(devInfo);
         }
         return true;
     }
+
 }
