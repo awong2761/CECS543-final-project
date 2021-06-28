a8b259329c3d0948f6a86fd098eab045366dfe46
ndwong36 <80723666+ndwong36@users.noreply.github.com>
2021-06-28 00:40:59 -0700

    ANIMATIONDONE

--- app/src/main/java/com/example/project1/Login.java
+++ app/src/main/java/com/example/project1/Login.java
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -39,12 +44,9 @@ public class Login extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private Context context;
     private ProgressDialog progressDialog;
+    private TextView loginlogotext;
     Data data = new Data();
-
-    // Splash stuff
-    Animation topAnimation, botAnimation;
-    ImageView logo;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +59,7 @@ public class Login extends AppCompatActivity {
         password = findViewById(R.id.Password);
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
+        loginlogotext = findViewById(R.id.loginlogotext);
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         if(user != null) {
@@ -68,16 +71,16 @@ public class Login extends AppCompatActivity {
             startActivity(login);
         }
 
-        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        botAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-        logo = findViewById(R.id.dumbellLogo);
-
-        logo.setAnimation(botAnimation);
-        email.setAnimation(topAnimation);
-        password.setAnimation(topAnimation);
-        btnSignup.setAnimation(topAnimation);
-        btnLogin.setAnimation(topAnimation);
-
+        loginlogotext = findViewById(R.id.loginlogotext);
+        TextPaint paint = loginlogotext.getPaint();
+        float width = paint.measureText(loginlogotext.getText().toString());
+        Shader textShader = new LinearGradient(0, 0, width, loginlogotext.getTextSize(),
+                new int[]{
+                        Color.parseColor("#3DBDB0"),
+                        Color.parseColor("#04B5CE"),
+                }, null, Shader.TileMode.CLAMP);
+        loginlogotext.getPaint().setShader(textShader);
+        loginlogotext.setTextColor(Color.parseColor("#3DBDB0"));
 
 
         // Login button leads to welcome activity page
---
+++ app/src/main/java/com/example/project1/splash_screen.java
@@ -0,0 +1,66 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextPaint;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash_screen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+    TextView logotext;
+    Animation topAnimation, botAnimation;
+    ImageView logosplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logotext = findViewById(R.id.logotext);
+        TextPaint paint = logotext.getPaint();
+        float width = paint.measureText(logotext.getText().toString());
+        Shader textShader = new LinearGradient(0, 0, width, logotext.getTextSize(),
+                new int[]{
+                    Color.parseColor("#3DBDB0"),
+                    Color.parseColor("#04B5CE"),
+                }, null, Shader.TileMode.CLAMP);
+        logotext.getPaint().setShader(textShader);
+        logotext.setTextColor(Color.parseColor("#3DBDB0"));
+
+        logosplash = findViewById(R.id.logodum);
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logotext.setAnimation(botAnimation);
+        logosplash.setAnimation(botAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, Login.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logosplash, "logo_image");
+                pairs[1] = new Pair<View, String>(logotext, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation
+                        (splash_screen.this, pairs);
+                startActivity(intent, options.toBundle());
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
